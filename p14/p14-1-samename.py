081fe6f5ccbdcac33c1eb86eb3e7cbcad22c1274
hyunlae <gusfot@gmail.com>
2023-07-12 16:52:58 +0900

    feat: 딕셔너리를 이용해 동명이인을 찾는 알고리즘

---
+++ p14/p14-1-samename.py
@@ -0,0 +1,27 @@
+# 두 번 이상 나온 이름 찾기
+# 입력: 이름이 n개 들어있는 리스트
+# 출력: n개의 이름 중 반복되는 이름의 집합
+
+def find_same_name(a):
+
+    name_dict = {}
+    for name in a:
+        if name in name_dict:
+            name_dict[name] += 1
+        else:
+            name_dict[name] = 1
+
+    result = set()
+
+    for name in name_dict:
+        if name_dict[name] > 1:
+            result.add(name)
+
+    return result
+
+
+name = ["Tom", "Jerry", "Mike", "Tom"]
+print(find_same_name(name))
+
+name2 = ["Tom", "Jerry", "Mike", "Tom", "Mike"]
+print(find_same_name(name2))
