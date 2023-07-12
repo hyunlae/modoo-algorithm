4f8e230d69ad2e122659f94427f905afd7727921
hyunlae <gusfot@gmail.com>
2023-07-12 10:24:43 +0900

    1부터 n까지 연속한숫자의 합을 구하는 알고리즘

---
+++ p01-1-sum.py
@@ -0,0 +1,15 @@
+# 1부터 n까지 연속한 숫자의 합을 구하는 알고리즘 1
+# 입력: n
+# 출력: 1부터 n까지의 숫자를 더한 값
+
+def sum_n(n):
+    sum = 0
+
+    for i in range(1, n + 1):
+        sum += i
+
+    return sum
+
+
+print(sum_n(10))
+print(sum_n(100))
